a19e22b0772697b20686b7fdf4948e2fc1200c4c
Tendercfj <1805150751@qq.com>
2026-02-13 17:46:36 +0800

    fix：修复根路径问题

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/MyResume/" : "/",
+  // base: process.env.NODE_ENV === "production" ? "/MyResume/" : "/",
+  base: "/",
   plugins: [
     vue(),
     createSvgIconsPlugin({
